4d040b951fad234f3293ea22076ab62cec986ec7
CongBinh24 <ncb240600@gmail.com>
2025-06-20 16:44:10 +0700

    Animation + Move player
    Add animation Run + idle, Add Script Player set movement for player

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float moveSpeed = 2f; 
+
+    private Vector3 moveInput = Vector3.zero;
+
+    private Rigidbody _rb;
+    private Animator _anim;
+    
+    
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal"); // A/D hoặc ← →
+        float v = Input.GetAxisRaw("Vertical");   // W/S hoặc ↑ ↓
+        moveInput = new Vector3(h, 0f, v);
+
+        bool isRuning = moveInput.magnitude > 0.1f;
+        _anim.SetBool(Const.RUN_ANIM, isRuning);
+
+        if (isRuning)
+        {
+            transform.forward = moveInput;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = moveInput.normalized * moveSpeed ;
+        _rb.MovePosition(_rb.position + move * Time.fixedDeltaTime);
+    }
+}
